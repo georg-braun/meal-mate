775d75955370a6b3f75a05fe3fd3d9e9b25fe721
Georg <Georg.Braun92@gmail.com>
2023-04-23 13:34:32 +0200

    Restructure

--- backend/application/Commands/Template/UpdateTemplateCommand.cs
+++ backend/application/Commands/Template/UpdateTemplateCommand.cs
@@ -33,47 +33,68 @@ public class UpdateTemplateCommandHandler : IRequestHandler<UpdateTemplateComman
         var existingTemplate = await _context.Templates.Include(_ => _.TemplateItems).ThenInclude(_ => _.Item).FirstOrDefaultAsync(_ => _.Id.Equals(request.Id), cancellationToken: cancellationToken);
         if (existingTemplate is null) throw new ArgumentException($"Couldn't find template with id {request.Id}.");
 
-        // Remove the item in the database if it's not in the request anymore
-        foreach (var existingTemplateItemId in existingTemplate.TemplateItems.Where(_ => _.Item != null).Select(_ => _.Item!.Id))
-        {
-      
-            if (request.Items.Select(_ => _.ItemId).Contains(existingTemplateItemId))
-            {
-                continue;
-            }
-            // todo: check if this is enough or the item should be removed explicitly from the database.
-            existingTemplate.RemoveTemplateItem(existingTemplateItemId);
-        }
+        RemoveTemplateItems(request, existingTemplate);
+        await UpdateTemplateItems(request, cancellationToken, existingTemplate);
+        await AddNewTemplateItems(request, cancellationToken, existingTemplate);
 
-        // Update the existing items
-        var templateItemsToUpdate = request.Items.Where(_ => _.ItemId != Guid.Empty).ToList();
-        foreach (var templateItemToUpdate in templateItemsToUpdate)
-        {
-            var item = await _context.Items.FindAsync(new object?[] { templateItemToUpdate.ItemId }, cancellationToken: cancellationToken);
-            existingTemplate.UpdateTemplateItem(templateItemToUpdate.Id, item, templateItemToUpdate.Name, templateItemToUpdate.Amount);
-        }
-        
-        var newTemplateItems = request.Items.Where(_ => _.ItemId == Guid.Empty).ToList();
-        foreach (var newTemplateItem in newTemplateItems)
-        {
-            // Try to get the item from the database
-            var item = await _context.Items.FindAsync(new object?[] { newTemplateItem.ItemId }, cancellationToken: cancellationToken);
-
-            // If the item is not in the database yet, create a new one.
-            if (item is null)
-            {
-                item = Item.Create(newTemplateItem.Name);
-                await _context.AddAsync(item, cancellationToken);
-            };
-            
-            existingTemplate.AddTemplateItem(item, newTemplateItem.Name, newTemplateItem.Amount);
-        }
-        
         existingTemplate.Name = request.Name;
         existingTemplate.Instructions = request.Instructions;
         
         await _context.SaveChangesAsync(cancellationToken);
 
         return existingTemplate;
+    }
+
+    private static void RemoveTemplateItems(UpdateTemplateCommand request, domain.Template existingTemplate)
+    {
+        // Remove the item in the database if it's not in the request anymore
+        foreach (var existingTemplateItemId in existingTemplate.TemplateItems.Where(_ => _.Item != null)
+                     .Select(_ => _.Item!.Id))
+        {
+            if (request.Items.Select(_ => _.ItemId).Contains(existingTemplateItemId))
+            {
+                continue;
+            }
+
+            // todo: check if this is enough or the item should be removed explicitly from the database.
+            existingTemplate.RemoveTemplateItem(existingTemplateItemId);
+        }
+    }
+
+    private async Task UpdateTemplateItems(UpdateTemplateCommand request, CancellationToken cancellationToken,
+        domain.Template existingTemplate)
+    {
+        // Update the existing items
+        var templateItemsToUpdate = request.Items.Where(_ => _.ItemId != Guid.Empty).ToList();
+        foreach (var templateItemToUpdate in templateItemsToUpdate)
+        {
+            var item = await _context.Items.FindAsync(new object?[] {templateItemToUpdate.ItemId},
+                cancellationToken: cancellationToken);
+            existingTemplate.UpdateTemplateItem(templateItemToUpdate.Id, item, templateItemToUpdate.Name,
+                templateItemToUpdate.Amount);
+        }
+    }
+
+    private async Task AddNewTemplateItems(UpdateTemplateCommand request, CancellationToken cancellationToken,
+        domain.Template existingTemplate)
+    {
+        var newTemplateItems = request.Items.Where(_ => _.ItemId == Guid.Empty).ToList();
+        foreach (var newTemplateItem in newTemplateItems)
+        {
+            // Try to get the item from the database
+            var item = await _context.Items.FindAsync(new object?[] {newTemplateItem.ItemId},
+                cancellationToken: cancellationToken);
+
+            // If the item is not in the database yet, create a new one.
+            if (item is null)
+            {
+                item = Item.Create(newTemplateItem.Name);
+                await _context.AddAsync(item, cancellationToken);
+            }
+
+            ;
+
+            existingTemplate.AddTemplateItem(item, newTemplateItem.Name, newTemplateItem.Amount);
+        }
     }
 }
