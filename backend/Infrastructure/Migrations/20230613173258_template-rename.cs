22b9f472e39b1f86b269e04fa03d0b39c409cbdc
Georg <Georg.Braun92@gmail.com>
2023-06-13 19:33:19 +0200

    rename column

---
+++ backend/Infrastructure/Migrations/20230613173258_template-rename.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class templaterename : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Amount",
+                table: "TemplateItem",
+                newName: "Qualifier");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Qualifier",
+                table: "TemplateItem",
+                newName: "Amount");
+        }
+    }
+}
