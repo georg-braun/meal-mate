6b4778cf72b5e86e3b2e8c93c853bb72d9153bb8
Georg <Georg.Braun92@gmail.com>
2023-07-13 22:18:16 +0200

    Use simple using syntax

--- backend/WebApi/MigrationManager.cs
+++ backend/WebApi/MigrationManager.cs
@@ -7,14 +7,10 @@ public static class MigrationManager
 {
     public static WebApplication MigrateDatabase(this WebApplication webApp)
     {
-        using (var scope = webApp.Services.CreateScope())
-        {
-            using (var appContext = scope.ServiceProvider.GetRequiredService<MealMateContext>())
-            {
-                // Ensure that the database is created and the migrations are applied
-                appContext.Database.Migrate();
-            }
-        }
+        using var scope = webApp.Services.CreateScope();
+        using var appContext = scope.ServiceProvider.GetRequiredService<MealMateContext>();
+        // Ensure that the database is created and the migrations are applied
+        appContext.Database.Migrate();
 
         return webApp;
     }
