ff7ca5d78d7cf2bf14f0044931c66dcd47c9de89
Georg <Georg.Braun92@gmail.com>
2023-08-27 07:37:38 +0200

    use icon library

--- frontend-skeleton/vite.config.ts
+++ frontend-skeleton/vite.config.ts
@@ -4,7 +4,12 @@ import Icons from 'unplugin-icons/vite'
 
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		Icons({
+			compiler: 'svelte',
+		})
+	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
