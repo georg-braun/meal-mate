96cf144796bfed28746e41435cc6745d6bda7e4f
Georg <Georg.Braun92@gmail.com>
2023-10-29 18:30:52 +0100

    Adds Import Shortcut

--- frontend-skeleton/vite.config.ts
+++ frontend-skeleton/vite.config.ts
@@ -1,6 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
-import Icons from 'unplugin-icons/vite'
+import Icons from 'unplugin-icons/vite';
+import path from 'path';
 
 
 export default defineConfig({
@@ -12,5 +13,10 @@ export default defineConfig({
 	],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	resolve: {
+		alias: [
+			{ find: '@src', replacement: path.resolve(__dirname, './src'), },
+		]
 	}
 });
